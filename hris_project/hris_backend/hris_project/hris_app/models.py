3bf9bd7bfe34438f3f24762da305e5a0f3e88ad0
Steven Lie <203203.steven@uib.edu>
2026-08-28 12:03:36 +0800

    test

--- hris_project/hris_backend/hris_project/hris_app/models.py
+++ hris_project/hris_backend/hris_project/hris_app/models.py
@@ -18,6 +18,8 @@ class Company(models.Model):
     company_code = models.CharField(max_length=100)
     address = models.TextField(null=True,blank=True)
     phone_number = models.CharField(max_length=20)
+    number = models.CharField(max_length=20)
+
 
     def __str__(self):
         return self.name
