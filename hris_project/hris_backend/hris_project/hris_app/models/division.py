5e73f12f3fad6197c323a4c0585f71cb7ecae29f
Steven Lie <203203.steven@uib.edu>
2026-09-11 17:04:22 +0800

    department zktbiotime sync

--- hris_project/hris_backend/hris_project/hris_app/models/division.py
+++ hris_project/hris_backend/hris_project/hris_app/models/division.py
@@ -4,7 +4,8 @@ from django.utils.translation import gettext_lazy as _
 
 class Department(models.Model):
     name= models.CharField(max_length=200,unique=True)
-    code = models.CharField(_("Code"), max_length=20, null=True, blank=True)
+    code = models.CharField(_("Department Code"), max_length=20, null=True, blank=True)
+    zk_id = models.BigIntegerField(_("ZKTeco Department ID"), null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -12,14 +13,17 @@ class Department(models.Model):
 class Section(models.Model):
     name=models.CharField(max_length=200)
     # department=models.ForeignKey(Department,on_delete=models.DO_NOTHING,null=True,blank=True)
-    code = models.CharField(_("Code"), max_length=20, null=True, blank=True)
+    code = models.CharField(_("Section Code"), max_length=20, null=True, blank=True)
+    zk_id = models.BigIntegerField(_("ZKTeco Section ID"), null=True, blank=True)
+
 
     def __str__(self):
         return self.name
 
 class Position(models.Model):
-  name = models.CharField(max_length=100, unique=True)
-  code = models.CharField(_("Code"), max_length=20, null=True, blank=True)
+   name = models.CharField(max_length=100, unique=True)
+   code = models.CharField(_("Postition Code"), max_length=20, null=True, blank=True)
+   zk_id = models.BigIntegerField(_("ZKTeco Position ID"), null=True, blank=True)
 
-  def __str__(self):
-    return self.name
+   def __str__(self):
+      return self.name
--- hris_project/hris_backend/hris_project/hris_app/views/zkteco_view.py
+++ hris_project/hris_backend/hris_project/hris_app/views/zkteco_view.py
@@ -2,32 +2,35 @@ import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from hris_app.models import Employee
+from hris_app.models import Employee, Department
 
-class SyncEmployeeToZkBiotimeView(APIView):
+class BiotimeLogin:
     ZK_BASE_URL = "http://10.106.13.48:8081"
     ZK_USERNAME = "api-zkteco"  # Ganti dengan username ZK BioTime
-    ZK_PASSWORD = "api-zkteco"  # Ganti dengan password ZK BioTime
-    print('mulai111')
-    def get_zk_token(self):
-        print('start111')
+    ZK_PASSWORD = "api-zkteco"
+
+    def get_token(self):
         login_url = f"{self.ZK_BASE_URL}/api-token-auth/"
         payload = {
             "username": self.ZK_USERNAME,
             "password": self.ZK_PASSWORD
         }
-        
-        try:
-            response = requests.post(login_url, json=payload, timeout=10)
-            if response.status_code == 200:
-                # Response BioTime mengembalikan: {"token": "eyJ0eXAiOi..."}
-                return response.json().get("token")
+
+        try :
+            print(1111)
+            login_response = requests.post(login_url, json=payload, timeout=10)
+            if login_response.status_code == 200:
+                return login_response.json().get("token")
             else:
-                print("Gagal Autentikasi ZK:", response.text)
+                print("Gagal Autentukasi ZK: ", login_response.text)
                 return None
         except requests.exceptions.RequestException as e:
-            print("Error Koneksi Login ZK:", str(e))
-            return None
+            return Response({"Gagal Login ke Server ZKBiotime : {e}"}, status=login_response.status)
+
+
+class SyncEmployeeToZkBiotimeView(APIView):
+  # Ganti dengan password ZK BioTime
+    zk_auth = BiotimeLogin()
 
     def post(self, request):
         employee_id = request.data.get("employee_id")
@@ -42,10 +45,10 @@ class SyncEmployeeToZkBiotimeView(APIView):
             return Response({"detail": "Karyawan tidak ditemukan."}, status=status.HTTP_404_NOT_FOUND)
 
         # 2. Dapatkan Token ZK BioTime Secara Otomatis
-        zk_token = self.get_zk_token()
+        zk_token = self.zk_auth.get_token()
         if not zk_token:
             return Response({
-                "detail": "Gagal melakukan autentikasi ke Server ZKTeco BioTime. Periksa Username/Password API ZK."
+                "detail": "Gagal melakukan autentikasi ke Server ZKTeco BioTime."
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         # 3. Buat Payload Karyawan untuk ZK BioTime
@@ -65,7 +68,7 @@ class SyncEmployeeToZkBiotimeView(APIView):
 
         # 5. Kirim Request Sync Server-to-Server
         try:
-            sync_url = f"{self.ZK_BASE_URL}/personnel/api/employees/"
+            sync_url = f"{BiotimeLogin.ZK_BASE_URL}/personnel/api/employees/"
             if employee.biometric_user_id :
                 update_url = f"{sync_url}{employee.biometric_user_id}/"
                 zk_response = requests.put(
@@ -106,3 +109,81 @@ class SyncEmployeeToZkBiotimeView(APIView):
             return Response({
                 "detail": f"Gagal terhubung ke Server ZKTeco: {str(e)}"
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class SyncDepartmentToZKBiotimeView(APIView):
+    zk_auth = BiotimeLogin()
+
+    def post(self, request):
+        department_id = request.data.get("department_id")
+
+        if not department_id:
+            return Response({"Detail Informasi Department Dari HRIS Tidak Ditemukan."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try : 
+            department = Department.objects.get(id=department_id)
+        except Department.DoesNotExist:
+            return Response({"Detail Informasi Department Dari HRIS Tidak Ditemukan"}, status=status.HTTP_404_NOT_FOUND)
+
+        zk_token = self.zk_auth.get_token()
+        if not zk_token:
+            return Response({"Gagal Melakukan Auntentikasi ke Sever ZKTeco BioTime"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        zk_id = department.zk_id if department.zk_id else None
+        zk_payload = {
+            'department_code': "" if zk_id else department.code, 
+            'department_name': department.name
+        }
+
+        header = {
+            "Authorization": f"Token {zk_token}",
+            "Content-Type": "application/json",
+        }
+
+        try:
+            print(2222)
+            sync_url = f"{BiotimeLogin.ZK_BASE_URL}/personnel/api/departments/"
+            if zk_id:
+                update_url = f"{sync_url}{zk_id}/"
+                zk_response = requests.put(
+                    update_url,
+                    json=zk_payload,
+                    headers=header,
+                    timeout=10
+                )
+                if zk_response.status_code in [200, 201]:
+                    return Response({"message": f"Berhasil sinkronisasi Department {department.name} ke ZKTeco BioTime!","zk_data": zk_response.json()}, status=status.HTTP_200_OK)
+                else :
+                    return Response({"message": f"Gagal Mengupdate / Sinkronisasi Department {department.name} ke ZKTeco Biotime!", "zk_data": zk_response.json()}, status=zk_response.status_code)
+            else :
+                zk_response = requests.post(
+                    sync_url,
+                    json=zk_payload,
+                    headers=header,
+                    timeout=10
+                )
+                if zk_response.status_code in [200, 201]:
+                    res_data = zk_response.json()
+                    res_id = ''
+                    if res_data and res_data.get('id'):
+                        res_id = res_data.get('id')
+                    department.zk_id = res_id
+                    department.save()
+                    return Response({
+                        "message": f"Berhasil sinkronisasi Department {department.name} ke ZKTeco BioTime!",
+                        "zk_data": zk_response.json()
+                    }, status=status.HTTP_200_OK)
+                else:
+                    return Response({
+                        "detail": "ZKTeco Menolak Request",
+                        "error": zk_response.json()
+                    }, status=zk_response.status_code)
+
+        except requests.exceptions.RequestException as e :
+            return Response({"detail": f"Gagal terhubung ke Server ZKTeco: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        
+
+
+    
+
